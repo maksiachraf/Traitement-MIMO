99266be374fae91be3d0533723c415f80fd4bbe0
Anas BAHARI <anasbahari5@gmail.com>
2021-11-30 21:42:47 +0100

    Code d'Alamouti

--- main_Alamouti.m
+++ main_Alamouti.m
@@ -8,7 +8,7 @@ M = 2;         % Le nombre d'antennes de receptions
 N = 2;         % Le nombre d'antennes d'emission
 L = 2;         % Le nombre de symboles
 nbs = 2;       % Nombre de bits/symbole 
-NbT = nbs*N*L; % Nombre de bits emis
+NbT = nbs*L;   % Nombre de bits emis
 Ns = 4;        % Le nombre de symboles dans la constellation
 
 Nr = 1000;     % Le nombre de realisations
@@ -18,13 +18,10 @@ SNR_max = 10;
 SNR_pas = 1;
 SNR = SNR_min:SNR_pas:SNR_max; % SNR en dB
 
+symb_constellation = [exp(1i*pi/4), exp(1i*3*pi/4), exp(1i*5*pi/4), exp(1i*7*pi/4)];
 % Generer H
 sigma_H = 1;
 H = sqrt(sigma_H/2)*( randn(M,N) + 1i*randn(M,N));
-
-% Generer C
-symb_constellation = [exp(1i*pi/4), exp(1i*3*pi/4), exp(1i*5*pi/4), exp(1i*7*pi/4)];
-matrix_C = construire_ensbl_mat(symb_constellation, N, L, Ns);
 
 %% Simulation de la transmission
 proba_err = zeros(1,length(SNR)); % Vecteur des taux d'erreur binaire
@@ -34,7 +31,12 @@ for j=1:length(SNR)
         %% Emetteur
         bits = randn(1,NbT) > 0; % Generation des bits
         S = modulateur_qpsk(bits, NbT, nbs); % Bits -> Symboles
-        X = reshape(S, N, L);
+        
+        % Code d'Alamouti
+        X(1,1) = S(1); 
+        X(2,2) = conj(S(1)); 
+        X(2,1) = S(2); 
+        X(1,2) = -conj(S(2)); 
         
         %% Canal
         sigma_V = 10^(-SNR(j)/10);
@@ -42,9 +44,21 @@ for j=1:length(SNR)
         Y = H*X + V;
         
         %% Recepteur
-%         S_rec = decodeur_ML(Y, H, matrix_C); %Decodage ML
-        S_rec = decodeur_ZF(Y, H, symb_constellation); %Decodage ZF
+        %------------------------------------------------------------------
+        z1 = conj(transpose(H(:,1)))*Y(:,1) + conj(transpose(Y(:,2)))*H(:,2); 
+        z2 = conj(transpose(H(:,2)))*Y(:,1) - conj(transpose(Y(:,2)))*H(:,1);  
+        norm_frob_car_H = norm(H,'fro')^2;
+        for i_symb=1:4
+            x1_chapeau(i_symb) = abs(z1-norm_frob_car_H*symb_constellation(i_symb))^2;
+            x2_chapeau(i_symb) = abs(z2-norm_frob_car_H*symb_constellation(i_symb))^2;
+        end
+        [~, argmin_x1_chapeau] = min(x1_chapeau);
+        [~, argmin_x2_chapeau] = min(x2_chapeau);
         
+        S_rec(1) = symb_constellation(argmin_x1_chapeau);
+        S_rec(2) = symb_constellation(argmin_x2_chapeau);
+        %------------------------------------------------------------------
+
         bit_rec = demodulateur_qpsk(S_rec); % Demodulation QPSK
         
         % Calcul du nombre d'erreur
@@ -68,12 +82,5 @@ ylim([1e-6 1])
 xlabel('SNR en dB', 'FontSize',14)
 ylabel("Probabilité d'erreur",'FontSize',14)
 grid on 
-
-ylim([10^(-2) 1])
-% legend('ML', 'ZF')
-
-%%%%%%
-%zf est plus rapide mais beaucoup moins performant.
-
-
+% ylim([10^(-2) 1])
 
--- modulateur_qpsk.m
+++ modulateur_qpsk.m
@@ -2,15 +2,16 @@ function [S] = modulateur_qpsk(bits, NbT, nbs)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
+    symb_constellation = [exp(1i*pi/4), exp(1i*3*pi/4), exp(1i*5*pi/4), exp(1i*7*pi/4)];
     for i=1:NbT/nbs
         if (bits(1,(i-1)*nbs+1:i*nbs) == [0 0])
-            S(1,i) = exp(1i*pi/4);
+            S(1,i) = symb_constellation(1);
         elseif (bits((i-1)*nbs+1:i*nbs) == [0 1])
-            S(1,i) = exp(1i*3*pi/4);
+            S(1,i) = symb_constellation(2);
         elseif (bits((i-1)*nbs+1:i*nbs) == [1 1])
-            S(1,i) = exp(1i*5*pi/4);
+            S(1,i) = symb_constellation(3);
         else
-            S(1,i) = exp(1i*7*pi/4);
+            S(1,i) = symb_constellation(4);
         end
     end
 end
